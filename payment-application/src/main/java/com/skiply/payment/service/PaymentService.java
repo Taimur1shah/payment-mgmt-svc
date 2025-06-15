919e3068a70c94042bbe390443595f3a38627084
taimur-shah <bitrtpl@123>
2025-06-15 16:17:13 +0400

    code update

--- payment-application/src/main/java/com/skiply/payment/service/PaymentService.java
+++ payment-application/src/main/java/com/skiply/payment/service/PaymentService.java
@@ -1,14 +1,11 @@
 package com.skiply.payment.service;
 
 import com.skiply.payment.domain.TransactionDetails;
+import com.skiply.payment.dto.PaymentDto;
 import java.util.Optional;
 
 public interface PaymentService {
-
-  void save(TransactionDetails transactionDetails);
-
-  Optional<TransactionDetails> getById(Integer id);
-
-  void deleteFee(Integer id);
-
+  TransactionDetails performTransaction(PaymentDto paymentDto);
+  TransactionDetails save(TransactionDetails transactionDetails);
+  Optional<TransactionDetails> getByReceiptId(Integer receiptId);
 }
